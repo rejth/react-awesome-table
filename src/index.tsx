09ca765bee84a41e202b9b73f70a6adbd0f749a8
Ilya Kirsanov <kirsanovia@yandex.ru>
2022-12-28 00:19:14 +0400

    fix: correct url

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
-import { LIGHT_THEME, FontsVTBGroup } from "@admiral-ds/react-ui";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { LIGHT_THEME, FontsVTBGroup } from '@admiral-ds/react-ui';
 
-import App from "src/ts/components/App";
-import TableDataService from "src/ts/services/TableDataService";
-import { TableDataServiceProvider } from "src/ts/contexts";
-import { endpoints } from "src/ts/api";
+import App from 'src/ts/components/App';
+import TableDataService from 'src/ts/services/TableDataService';
+import { TableDataServiceProvider } from 'src/ts/contexts';
+import { endpoints } from 'src/ts/api';
 
-import "./styles/index.scss";
+import './styles/index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 let apiService = new TableDataService();
 
 async function bootstrap() {
-  if (process.env.NODE_ENV === "development") {
-    if (window.location.pathname === "/react-custom-table") {
-      window.location.pathname = "/react-custom-table/";
+  if (process.env.NODE_ENV === 'development') {
+    if (window.location.pathname === '/react-custom-table') {
+      window.location.pathname = '/react-custom-table/';
       return;
     }
-    const { worker } = require("./mocks/browser");
+    const { worker } = require('./mocks/browser');
     apiService = endpoints;
     await worker.start({
       serviceWorker: {
-        url: "/react-custom-table/mockServiceWorker.js",
+        url: '/react-custom-table/mockServiceWorker.js',
       },
     });
   }
@@ -37,7 +37,7 @@ async function bootstrap() {
           <App />
         </TableDataServiceProvider>
       </ThemeProvider>
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 }
 
